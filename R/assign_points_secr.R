6e6d1cab6ee288865d076756f175c3e497e21988
Jolene Sutton <jolene.sutton@gmail.com>
2024-11-19 11:22:23 -0400

    new function

---
+++ R/assign_points_secr.R
@@ -0,0 +1,65 @@
+#' Assign points to polygon using package secr
+#'
+#' Before applying function, make sure that the CRS of the polygon matches the CRS of the data points.
+#' Returns a dataframe with nrow = df. There is a column for each polygon with 1 or 0 indicating
+#' whether each point falls within the polygon. There is also a column to indicate which polygon(s) each
+#' point has been assigned to. USE THIS INSTEAD OF `assign_points_terra` WHEN THERE ARE OVERLAPPING POLYGONS.
+#' @returns Returns a dataframe with nrow = df. There is a column for each polygon with 1 or 0 indicating
+#' whether each point falls within the polygon. There is also a column to indicate which polygon(s) each
+#' point has been assigned to.
+#' @param x X coordinate (e.g., longitude) with same CRS as polygon
+#' @param y Y coordinate (e.g., latitude) with same CRS as polygon
+#' @param polygon Spatvector with geometry polygons
+#' @param polygon.label The layer containing the name of each polygon in the Spatvector
+#' @returns Data frame
+#' @examples
+#' #poly<-get_shapefile('nafo.clipped')
+#' #df2<-assign_points_secr(dat.ziff[1:50,'longitude'],dat.ziff[1:50,'latitude'],poly,"Label")
+#' #' head(df2)
+#' @seealso assign_points_terra, dat.ziff, get_shapefile
+#' @export
+assign_points_secr<-function(x,y,polygon,polygon.label){
+
+  df<-cbind.data.frame(x,y)
+  names(df)<-c('x','y')
+
+  n<-nrow(df)
+  chunk_size <- 1000L
+  actual_chunk_sizes <- rep(1000, n %/% chunk_size)
+  if(n %% chunk_size != 0){actual_chunk_sizes <- c(actual_chunk_sizes, n %% chunk_size)}
+
+  my.list<-list()
+  for(j in seq_along(actual_chunk_sizes)){
+
+    ROWS<-seq_len(actual_chunk_sizes[j]) + (j-1)*chunk_size
+    #COLS<-1:length(unlist(terra::values(polygon[,which(names(polygon) %in% polygon.label)])))
+    COLS<-1:length(unlist(terra::values(polygon[,which(names(polygon) %in% polygon.label)])))
+    out <- matrix(nrow = length(ROWS),ncol = length(COLS))
+
+
+    writeLines(paste0("Processing points ",min(ROWS)," to ",max(ROWS),". ",format(Sys.time(), "%X")  ))
+
+    for (i in 1:ncol(out)) {
+      TMP <- as.data.frame(terra::geom(polygon[i, ]))
+      TMP <- TMP[which(TMP$hole %in% 0), c("x", "y")]
+
+      index <- which(secr::pointsInPolygon(df[ROWS, c('x','y')],TMP, logical = TRUE) %in% TRUE)
+
+      if (length(index > 0)) {
+        out[index, i] <- 1
+        out[-index, i] <- 0
+      }
+      if (length(index) == 0) {
+        out[, i] <- 0
+      }
+      out <- as.data.frame(out)
+      names(out) <- unlist(terra::values(polygon[, which(names(polygon) %in% polygon.label)]))
+    }
+    out$assigned.polygon <- apply(out == "1", 1, FUN = function(x) toString(names(x)[x]))
+    out[which(out$assigned.polygon == ""), "assigned.polygon"] <- NA
+    #my.list[[j]]<-cbind.data.frame(  DATA[ROWS,] , out$assigned.polygon)
+    my.list[[j]]<-out
+  }
+  OUT<-as.data.frame(do.call("rbind", my.list))
+  return(OUT)
+}
---
+++ R/make_grid.R
@@ -0,0 +1,50 @@
+#' Make a prediction grid for the Gulf Region
+#'
+#' Returns a data frame with coordinates that are equidistant from one another (raster grid cells), along
+#' with their associated NAFO divisions for 3Pn4RSTVn.
+#' The coordinates are the centers of the grid cells.
+#' Longitude and latitude coordinates are NAD83, EPSG:4269, in decimal degrees. X and Y coordinates are NAD83, UTM zone 20N, in km.
+#' @param res User defined grid resolution e.g., specifying 4 will produce 4x4km grid sizes
+#' @importFrom terra project vect aggregate extract
+#' @importFrom raster raster rasterize
+#' @importFrom sf st_as_sf st_coordinates st_transform
+#' @importFrom dplyr select filter
+#' @importFrom sp SpatialPoints spTransform coordinates
+#' @importFrom ggplot2 ggplot geom_raster
+#' @seealso get_shapefile()
+#' @examples
+#' #pgrid<-make_grid(20)
+#' #ggplot()+geom_raster(data=pgrid,aes(X,Y,fill=nafo.assigned))
+#' @export
+make_grid<-function(res){
+
+  shape<-get_shapefile('nafo.clipped')#SHAPEFILE
+
+  ## Make raster grids
+  pa0 <- terra::aggregate(shape)
+  shape2<-sf::st_as_sf(pa0)
+  shapeutm<-sf::st_transform (shape2, crs='+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs')#crs="+proj=utm +zone=20 +datum=WGS84 +units=km")
+
+  r <- raster::raster(as(shapeutm, "Spatial"), resolution = res)
+  rr <- raster::rasterize(as(shapeutm, "Spatial"), r, getCover = T)
+
+  pagrid <- as.data.frame(raster::rasterToPoints(rr))
+  a <- pagrid[,3] * res * res
+  pagrid$area<-a
+  names(pagrid)<-c('x','y','layer','area')
+  pagrid <- dplyr::filter(pagrid, area > 0) |>
+    dplyr::select(-layer)
+
+  sputm <- sp::SpatialPoints(pagrid, proj4string=sp::CRS("+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs"))
+  spgeo <- sp::spTransform(sputm, sp::CRS("+proj=longlat +datum=NAD83 +no_defs"))
+
+  lnlt <- sp::coordinates(spgeo)
+  pagrid2<-cbind.data.frame(pagrid,lnlt[,1:2])
+  names(pagrid2)<-c('X',"Y","area","longitude","latitude")
+
+  # Assign nafo
+  writeLines(paste0("Assigning NAFO divisions to ",nrow(pagrid2) ," grid cells."))
+  pagrid2$nafo.assigned<-assign_points_secr(pagrid2$longitude,pagrid2$latitude,shape,'Label')$assigned.polygon
+  pagrid2[pagrid2==""]<-NA
+  return(pagrid2)
+  }
