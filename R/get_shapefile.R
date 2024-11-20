f689c43e83f75f4033d63f0ee8165a288ddec013
Jolene Sutton <jolene.sutton@gmail.com>
2024-11-20 10:43:56 -0400

    new shapefiles

--- R/get_shapefile.R
+++ R/get_shapefile.R
@@ -1,7 +1,7 @@
 #' Retrieve a stored shapefile
 #'
 #' Imports a stored shapefile as a SpatVector (terra).
-#' @param x Either "nafo", "nafo.clipped", "coast", "rv.sgsl", "rv.ngsl", "rv.winter", "closures","hex". The "nafo" shapefile is not clipped to land. The "nafo.clipped" shapefile is clipped to land. The 'hex' shapefile is a hexagonal grid used in the fisheriescape project.
+#' @param x Either "nafo", "nafo.clipped", "mpa", "oecm","hex". The "nafo" shapefile is not clipped to land. The "nafo.clipped" shapefile is clipped to land. The 'hex' shapefile is a hexagonal grid used in the fisheriescape project.
 #' @importFrom terra vect
 #' @importFrom terra project
 #' @importFrom terra plot
@@ -13,20 +13,31 @@
 get_shapefile<-function(x){
 
   if(x == 'nafo.clipped'){
-    POLY<-terra::vect(system.file('extdata','nafo/NAFO_Divisions_2021_poly_clipped.shp',package='gslSpatial'))
-    POLY<-POLY[which(POLY$Division%in%c("4T","4V","4R","4S","3P")),]
-    POLY<-POLY[-which(POLY$SubDiv%in%c("4Vs","3Ps")),]
-    POLY<-terra::project(POLY,'epsg:4269')
+    POLY<-terra::vect(system.file('extdata','nafo_clipped/nafo_clipped.shp',package='gslSpatial'))
     writeLines("Source: https://www.nafo.int")
   }
 
   if(x == 'nafo'){
-    POLY<-terra::vect(system.file('extdata','nafo/NAFO_Divisions_2021_poly_not_clipped.shp',package='gslSpatial'))
-    POLY<-POLY[which(POLY$Division%in%c("4T","4V","4R","4S","3P")),]
-    POLY<-POLY[-which(POLY$SubDiv%in%c("4Vs","3Ps")),]
-    POLY<-terra::project(POLY,'epsg:4269')
+    POLY<-terra::vect(system.file('extdata','nafo_unclipped/nafo_unclipped.shp',package='gslSpatial'))
     writeLines("Source: https://www.nafo.int")
   }
+
+  if(x == 'mpa'){
+    writeLines("Oceans Act Marine Protected Areas, source:https://open.canada.ca")
+    POLY<-terra::vect(system.file('extdata','mpa_clipped/mpa_clipped.shp',package='gslSpatial'))
+  }
+
+  if(x == 'oecm'){
+    writeLines("Other Effective Area-Based Conservation Measures, source:https://open.canada.ca")
+    POLY<-terra::vect(system.file('extdata','oeabcm_clipped/oeabcm_clipped.shp',package='gslSpatial'))
+  }
+
+  if(x == 'hex'){
+    writeLines("10 x 10 km hex grid")
+    POLY<-terra::vect(system.file('extdata','hex_clipped/hex_clipped.shp',package='gslSpatial'))
+  }
+
+
 
   return(POLY)
 }
