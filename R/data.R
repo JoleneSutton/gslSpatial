bae5123474d45430387e910f0c2cb1d936a86786
Jolene Sutton <jolene.sutton@gmail.com>
2025-06-13 13:58:51 -0300

    update data description

--- R/data.R
+++ R/data.R
@@ -96,7 +96,7 @@
 
 #' Hexagonal grid, 10x10km
 #'
-#' A shapefile used in the fisheriescape project. CRS = ESRI:102001. Accessed with get_shapefile('hex')
+#' A shapefile used in the fisheriescape project, and sourced from: https://gcgeo.gc.ca/geonetwork/metadata/eng/572f6221-4d12-415e-9d5e-984b15d34da4. CRS = ESRI:102001. Accessed with get_shapefile('hex')
 #'
 #' @format Retrieved as class SpatVector with get_shapefile('hex')
 "hex"
